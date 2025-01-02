63ec3afdb4a519804ce2ac8aec375fb25ac61b17
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2025-01-02 23:45:03 +0500

    add classes

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,16 +1,53 @@
-import Appcard from "../components/card";
-import Applayout from "../components/layout";
+// import Appcard from "../components/card";
+// import Applayout from "../components/layout";
+import {Component} from 'react';
 
-const Dashboard = () => {
+class Button extends Component {
+  constructor(){
+     super()
+    this.state = {
+    myName : "mariyam"
+    }
+    
+  }
+  abc(){
+console.log("hi");
+
+  }
+render(){
+ 
+  const {children} = this.props;
+  return (
+  // <button onClick={this.abc}>{children}</button>
+  <h1>{this.state.myName}</h1>
+  )
+}
+}
+class Dashboard extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
     return (
-    <div className="container">
-       <Applayout title='smit'>
-        <div>
-        <Appcard/>  
-        </div>
-       </Applayout>
-  </div>
+      <div>
+        <Button>click me</Button>
+        <Button>update me</Button>
+      </div>
     )
   }
+}
+
+// const Dashboard = () => {
+//     return (
+//     <div className="container">
+//        <Applayout title='smit'>
+//         <div>
+//         <Appcard/>  
+//         </div>
+//        </Applayout>
+//   </div>
+//     )
+//   }
   
   export default Dashboard;
